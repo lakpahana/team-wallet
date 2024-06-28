1087851223fbd429075a566213f1a740bebe2700
AGS Lakpahana <agslakpahana@gmail.com>
2024-06-28 15:37:47 +0530

    jhfhfh

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 //const API = axios.create({ baseURL: 'http://localhost:3001'})
 //fdfdf
-const API = axios.create({ baseURL: 'http://35.168.14.244:3002' })
+const API = axios.create({ baseURL: 'http://18.204.10.28:3002' })
 
 const profile = JSON.parse(localStorage.getItem('profile'))
 
